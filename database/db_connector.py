fecd829c6770d9187faa0b6666aeaa8cfed49acd
Ellis Pax <55385346+ellispax@users.noreply.github.com>
2023-06-14 08:04:31 +0700

    Update db_connector.py

--- database/db_connector.py
+++ database/db_connector.py
@@ -4,24 +4,13 @@ import time as t
 import datetime as dt
 
 
-# def getConnect():
-
-#     mydb = db.connect(
-#         host = "sql12.freesqldatabase.com",
-#         user= "sql12592016",
-#         password="ngsdriE1iD",
-#         database="sql12592016")
-#     return mydb
-
-
 def getConnect():
 
     mydb = db.connect(
-        host = "irrigation.cwikznmtsvgr.us-east-1.rds.amazonaws.com",
-        user= "admin",
-        password="ATEyspy2QIG)",
-        database="irrigationdb")
+        host = "",
+        user= "root",
+        password="",
+        database="",
+        port = 3306
+    )
     return mydb
-
-DB = getConnect()
-print(DB)
